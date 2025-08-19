d9835f4539bb1a796e85d3e39fa16831769b75fe
Abhi Singh <156217394+urfatherabhi@users.noreply.github.com>
2025-08-19 21:45:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ API_HASH = "09bd2e64c04dc911d0bc45648d236fab"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "7907019701"
+OWNER_ID = "7556244377"
 SUPPORT_GRP = "@imagine_iq"
 UPDATE_CHNL = "@imagine_iq"
-OWNER_USERNAME = "ceo_of_secularism"
+OWNER_USERNAME = "URFather_ABHI"
 
